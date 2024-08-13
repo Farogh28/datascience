f76ccf6da5175ee93c67aa59785ca2f3320f755a
Farogh <farogh28@gmail.com>
2024-08-13 17:17:06 +0530

    python test project

--- Unspervised learning/k-means/K_means_algo/k_means.py
+++ Unspervised learning/k-means/K_means_algo/k_means.py
@@ -1,59 +1,57 @@
 import random
 import numpy as np
 
+
 class MyKmeansAlgo:
-    def __init__(self, n_cluster =3, max_itteration = 100):
+    def __init__(self, n_cluster=3, max_itteration=100):
         self.n_cluster = n_cluster
-        self.max_itteration  = max_itteration
+        self.max_itteration = max_itteration
         self.centroid = None
 
     def fit_predict(self, dataset):
         centroid_index = random.sample(range(dataset.shape[0]), self.n_cluster)
         dataset = dataset.values
+        # print(type(dataset))
         self.centroid = dataset[centroid_index]
 
-
-        x = 0 
+        x = 0
         for i in range(self.max_itteration):
-            x = x+1
+            x += 1
 
-            #3) Assign Cluster
+            # 3) Assign Cluster
             group_cluster = self.assign_cluster(dataset)
 
-            #4) Move Centroid
+            # 4) Move Centroid
             new_centroids = self.move_centroid(dataset, group_cluster)
             old_centroids = self.centroid
             self.centroid = new_centroids
 
-            #5) Finish
-            if(old_centroids == self.centroid).all():
+            # 5) Finish
+            if (old_centroids == self.centroid).all():
                 break
 
-        return group_cluster, self.centroid,x
-    
+        return group_cluster, self.centroid, x
+
     def assign_cluster(self, dataset):
         cluster_group = []
         distance = []
         for i in dataset:
             for centroid in self.centroid:
                 distance.append(np.sqrt(np.dot(i - centroid, i - centroid)))
-            print(distance)
+            # print(distance)
             min_distance = min(distance)
             cluster_group.append(distance.index(min_distance))
             distance.clear()
         return np.array(cluster_group)
-    
+
     def move_centroid(self, dataset, group_cluster):
         new_centroid = []
         cluster_type = np.unique(group_cluster)
 
-
         for type in cluster_type:
-            new_centroid.append(dataset[group_cluster == type].mean(axis = 0))
+            new_centroid.append(dataset[group_cluster == type].mean(axis=0))
 
         return np.array(new_centroid)
-
-
 
 
 # -------------------------------------------------------
@@ -74,3 +72,14 @@ class MyKmeansAlgo:
 
 
 # -------------------------------------------------------------
+
+    def calculate_wcss(self, dataset):
+        dataset = dataset.values
+        group_cluster = self.assign_cluster(dataset)
+        # print(group_cluster)
+        wcss = 0
+
+        for i, cluster in enumerate(group_cluster):
+            wcss += np.sum((dataset[i] - self.centroid[cluster]) ** 2)
+
+        return wcss
--- Unspervised learning/k-means/K_means_algo/use.py
+++ Unspervised learning/k-means/K_means_algo/use.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from k_means import MyKmeansAlgo
-
 df = pd.read_csv("data.csv")
 my_predictor = MyKmeansAlgo()
 new_dataset = df.drop(columns=["color"])
@@ -10,7 +9,7 @@ new_dataset = df.drop(columns=["color"])
 group_cluster,centroid,x = my_predictor.fit_predict(new_dataset)
 
 
-print(x)
+# print(x)
 
 plt.scatter(df["x"], df["y"])
 plt.scatter(new_dataset[group_cluster == 0]["x"], new_dataset[group_cluster == 0]["y"])
@@ -19,10 +18,10 @@ plt.scatter(new_dataset[group_cluster == 2]["x"], new_dataset[group_cluster == 2
 
 
 
-plt.scatter(centroid[0][0], centroid[0][1], s = 300)
-plt.scatter(centroid[1][0], centroid[1][1], s = 300)
-plt.scatter(centroid[2][0], centroid[2][1], s = 300)
-plt.show()
+# plt.scatter(centroid[0][0], centroid[0][1], s = 300)
+# plt.scatter(centroid[1][0], centroid[1][1], s = 300)
+# plt.scatter(centroid[2][0], centroid[2][1], s = 300)
+# plt.show()
 
 
 # plt.plot(range(1, max_k + 1), wcss, marker='o')
@@ -31,7 +30,19 @@ plt.show()
 # plt.ylabel('WCSS')
 # plt.show()
 
+wcss = []
+max_k = 10
+for j in range(1, max_k + 1):
+    kmeans = MyKmeansAlgo(n_cluster=j)
+    kmeans.fit_predict(new_dataset)
+    wcss.append(kmeans.calculate_wcss(new_dataset))
 
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, max_k + 1), wcss, marker='o')
+plt.title('Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
 
 # ---------------------------------------------------------
 
